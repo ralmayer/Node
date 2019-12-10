88d40cf15e37f7376e995b47325bee31339db03e
Ral Mayer <llev4n@gmail.com>
2019-12-10 15:28:20 +0400

    added some new controller functions & views

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -29,6 +30,19 @@ AuthorSchema
 
   return fullname;
 });
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return moment(this.date_of_birth).format('MMMM Do, YYYY');
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return moment(this.date_of_death).format('MMMM Do, YYYY');
+});
+
 
 // Virtual for author's lifespan
 AuthorSchema
