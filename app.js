da9b58f07472a0b8f58f0539d06d83ed1bce3ce3
Ral Mayer <llev4n@gmail.com>
2019-12-15 15:29:40 +0400

    created genre create form

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
