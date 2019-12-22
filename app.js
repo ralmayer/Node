64dfa98c149e274322604505fc5d36a802ca02ba
Leon (Ral) Mayer <48362631+ralmayer@users.noreply.github.com>
2019-12-22 16:09:51 +0400

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://amygdala:kevinshields@cluster0-9lp4q.mongodb.net/local_library?retryWrites=true'
+var dev_db_url = 'mongodb+srv://****/local_library?retryWrites=true'
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
